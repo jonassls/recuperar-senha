be065106fc5330657bb013b93c081be28d55efab
Jonas <jonas.2022310916@aluno.iffar.edu.br>
2024-06-14 15:06:50 -0300

    Texto pequeno

--- form-recuperar-senha.php
+++ form-recuperar-senha.php
@@ -8,6 +8,9 @@
 </head>
 
 <body>
+    Digite seu email para que você possa criar uma nova senha. <br>
+    Será enviado um email com um link de recuperação que você usará
+    para criar uma nova senha.<br><br>
     <form action="recuperar.php" method="post">
         <label>Email: <input type="email" name="email"></label><br>
         <input type="submit" value="Enviar email de recuperação">
