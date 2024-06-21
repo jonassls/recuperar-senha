5d092189596ee174c6f5555ad2a4f2833eb5d459
Jonas <jonas.2022310916@aluno.iffar.edu.br>
2024-06-21 13:34:20 -0300

    1

--- recuperar.php
+++ recuperar.php
@@ -1,4 +1,9 @@
 <?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
 require_once "conexao.php";
 $conexao = conectar();
 $email = $_POST['email'];
@@ -13,4 +18,26 @@ if ($usuario == null) {
 }
 //gerar um token unico
 $token = bin2hex(random_bytes(50));
-?>
+
+require_once 'PHPMailer/src/PHPMailer.php';
+require_once 'PHPMailer/src/SMTP.php';
+require_once 'PHPMailer/src/Exception.php';
+
+$mail = new PHPMailer(true);
+
+try {
+    $mail->CharSet= 'UTF-8';
+    $mail->Encoding= 'base64';
+    $mail->setLanguage('br');
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = '';
+    $mail->Password = '';
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = 587;
+} catch (Exception $e) {
+    echo "Não foi possível enviar o email.
+    Mailer Error:{$mail->ErrorInfo}";
+}
