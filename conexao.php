149921aaa0c3aea861a7ee163c6eeee5a1d51de3
Jonas <jonas.2022310916@aluno.iffar.edu.br>
2024-06-24 15:58:11 -0300

    1

--- conexao.php
+++ conexao.php
@@ -8,7 +8,8 @@
  */
 function conectar()
 {
-    $conexao = mysqli_connect("localhost", "root", "", "recuperar-senha");
+    require_once "config.php";
+    $conexao = mysqli_connect($config['host'],$config['user'], $config['pass'], $config['db']);
     if ($conexao === false) {
         echo "Erro ao conectar à base dados. Nº do erro:" . mysqli_connect_errno() . "." .
             mysqli_connect_error();
--- recuperar.php
+++ recuperar.php
@@ -22,21 +22,49 @@ $token = bin2hex(random_bytes(50));
 require_once 'PHPMailer/src/PHPMailer.php';
 require_once 'PHPMailer/src/SMTP.php';
 require_once 'PHPMailer/src/Exception.php';
+include 'config.php';
 
 $mail = new PHPMailer(true);
 
 try {
-    $mail->CharSet= 'UTF-8';
-    $mail->Encoding= 'base64';
+    $mail->CharSet = 'UTF-8';
+    $mail->Encoding = 'base64';
     $mail->setLanguage('br');
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;
     $mail->isSMTP();
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
-    $mail->Username = '';
-    $mail->Password = '';
+    $mail->Username = $config['email'];
+    $mail->Password = $config['senha-email'];
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
     $mail->Port = 587;
+    $mail->SMTPOptions = array(
+        'ssl' => array(
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+            'allow_self_signed' => true
+        )
+        );
+
+    //Recepients
+    $mail->setFrom($config['email'], 'Aula de Tópicos');
+    $mail->addAddress($usuario['email'], $usuario['nome']);
+    $mail->addReplyTo($config['email'], 'Aula de Tópicos');
+
+    //content
+    $mail->isHTML(true);
+    $mail->Subject = 'Recuperação de Senha do Sistema';
+    $mail->Body = 'Olá! <br>
+        Você solicitou a recuperação da sua conta no nosso sitema.
+        Para isso, clique no Link abaixo para realizar a troca de senha:<br>
+        <a href="'. $_SERVER['SERVER_NAME']. '/nova-senha.php?email='. $usuario['email']. '&token=' . $token . '">
+        Clique aqui para recuperar o acesso à sua conta!</a><br>">
+        <br>
+        Atenciosamente<br>
+        Equipe do sistema...';
+    $mail->send();
+    echo 'Email enviado com sucesso!<br> Confira seu email.';
+
 } catch (Exception $e) {
     echo "Não foi possível enviar o email.
     Mailer Error:{$mail->ErrorInfo}";
