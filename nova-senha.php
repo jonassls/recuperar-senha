634fb107f1658a334933a0832836d819fc3423a2
Jonas <jonas.2022310916@aluno.iffar.edu.br>
2024-07-15 16:30:23 -0300

    ;;;

--- nova-senha.php
+++ nova-senha.php
@@ -21,7 +21,28 @@ if ($recuperar == null) {
     $umDia = DateInterval::createFromDateString('1 Day');
     $dataExpiracao = date_add($data_criacao, $umDia);
 
-    if ($agora < $dataExpiracao) {
-        
+    if ($agora > $dataExpiracao) {
+        echo "Essa solicitação de recuperação de senha expirou!
+    Faça um novo pedido de recuperação de senha.";
+        die();
     }
 }
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nova Senha</title>
+</head>
+<body>
+    <form action="salvar-nova-senha.php" method="post">
+        <input type="hidden" name="email" value="<?=$email?>">
+        <input type="hidden" name="token" value="<?=$token?>">
+        Email: <?= $email?> <br>
+        <label>Senha: <input type="password" name="senha"></label><br>
+        <label>Repita a senha: <input type="password" name="repetirSenha"></label><br>
+        <input type="submit" value="Salvar nova senha">
+    </form>
+</body>
+</html>
